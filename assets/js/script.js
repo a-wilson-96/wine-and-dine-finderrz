a0b8f7603275c7972d3d688483f42864357865bb
Amanda Pietsch <amanda.ann.pietsch@gmail.com>
2022-11-28 11:33:02 -0800

    added pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-User Inputs 
+//  User Inputs Cuisine and Alcohol 
+
+THEN user clicks submit button 
+    IF both inputs are not received
+        ALERT both inputs are required  
+    IF both inputs are received 
+        SHOW cuisine and cocktail ingredients
+        SHOW image of cuisine and cocktail 
+
+IF user clicks generate new pairing 
+    THEN random pairing will generate
+
+IF user clicks save your pairing 
+    ALERT this pairing has been saved    
+    THEN current selection is saved to list on local storage
+
+CLICK user clicks previous pairings
+    THEN user is directed to the list on the local storage  
