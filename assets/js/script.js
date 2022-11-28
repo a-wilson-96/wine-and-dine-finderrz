fcff4257d509bba4542a67fac790faf6356653ab
Aaron Wilson <aaronmwilson96@gmail.com>
2022-11-28 10:37:07 -0900

    tested API functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-User Inputs 
+var queryURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Italian'
+
+fetch(queryURL)
+.then(function (res) {
+  return res.json();
+})
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.log(err);
+});
