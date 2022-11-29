bda147b9de18c96e4d8b6adf405f5470d11ecd1f
talexander91 <116913084+talexander91@users.noreply.github.com>
2022-11-29 14:04:14 -0800

    added model on save click

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,7 +64,7 @@ var handleFormSubmit = function(event) { //pressing the submit button will run t
   console.log(drinkChoice);
   fetchData(getNationChoice(nationChoice), getDrink(drinkChoice));
   console.log("handleFormSubmit complete")
-  
+  document.getItem
 }
 
 
@@ -102,3 +102,28 @@ submit.addEventListener("click", handleFormSubmit)
   // searchForm.addEventListener('submit', getNationChoice);
   // searchForm.addEventListener('submit', getDrink);
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("save-pairing");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
