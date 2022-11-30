fad16315a49f1df6d54c7359de4b58baac3e3def
honeytech <honey93k@gmail.com>
2022-11-29 18:00:59 -0800

    added save button variable and modified html

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,8 +67,6 @@ var handleFormSubmit = function(event) { //pressing the submit button will run t
 
 }
 
-
-
 function fetchData(nation, base) {
   console.log("starting fetchData")
 fetch(nation) //food by nation api
@@ -114,12 +112,18 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("save-pairing");
 
+var saveBtn = document.getElementById("savebtn")
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+}
+
+function show() {
+  document.getElementById('save-pairing').style.display="none"
 }
 
 // When the user clicks on <span> (x), close the modal
